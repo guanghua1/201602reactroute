f67dfb52e1e6d2a35b00616ae8096dc5ec42aebb
zhufengnodejs <zhufengnodejs@126.com>
2016-06-23 16:22:00 +0800

    默认路由

--- app/index.js
+++ app/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App,Home,User,Profile } from './containers';
 import {UserAdd,UserList,UserDetail} from './components';
-import {Router,Route,hashHistory} from 'react-router';
+import {Router,Route,hashHistory,IndexRoute,Redirect} from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
 
 let root = document.getElementById('app');
 render( <Router history={hashHistory} >
     <Route path="/" component={App}>
+        <IndexRoute component={Home}/>
         <Route path="home" component={Home}/>
         <Route path="user" component={User}>
-            <Route path="list" component={UserList}/>
+            <IndexRoute component={UserList}/>
+            <Route path="/list" component={UserList}/>
+            <Redirect from="list" to="/list" />
             <Route path="add" component={UserAdd}/>
             <Route path="detail/:id" component={UserDetail}/>
         </Route>
