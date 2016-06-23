661698935456531bc58c82de3519563a50316534
zhufengnodejs <zhufengnodejs@126.com>
2016-06-23 12:46:56 +0800

    增加导航链接

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-
+import {Link} from 'react-router';
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <nav className="navbar navbar-default">
           <div className="container-fluid">
-            <div className="collapse navbar-collapse">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">珠峰培训</a>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav navbar-nav">
-                <li className="active">首页</li>
-                <li className="active">用户管理</li>
-                <li className="active">个人设置</li>
+                <li className="active"><Link to="/home">首页</Link></li>
+                <li><Link to="/user">用户管理</Link></li>
+                <li><Link to="/profile">个人设置</Link></li>
               </ul>
             </div>
           </div>
